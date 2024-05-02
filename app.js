f5daec4da109131283745e7874c1080204dca8d7
SalahJokhio <154239945+SalahJokhio@users.noreply.github.com>
2024-05-03 01:06:35 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+var clock = document.getElementById("clock");
+var hoursElement = document.getElementById("hours");
+var minutesElement = document.getElementById("minutes");
+var secondsElement = document.getElementById("seconds");
+
+setInterval(function() {
+    var currentDate = new Date();
+    var currentHour = currentDate.getHours();
+    var currentMinutes = currentDate.getMinutes();
+    var currentSeconds = currentDate.getSeconds();
+
+    // Add leading zero for single-digit hours, minutes, and seconds
+    currentHour = currentHour < 10 ? '0' + currentHour : currentHour;
+    currentMinutes = currentMinutes < 10 ? '0' + currentMinutes : currentMinutes;
+    currentSeconds = currentSeconds < 10 ? '0' + currentSeconds : currentSeconds;
+
+    hoursElement.innerText = currentHour;
+    minutesElement.innerText = currentMinutes;
+    secondsElement.innerText = currentSeconds;
+
+    clock.classList.toggle("flash-effect", currentSeconds % 2 === 0);
+}, 1000);
+
+
+
+
+
+
+
+// // Get the clock element
+// const clock = document.getElementById('clock');
+
+// // Function to toggle the hover effect every second
+// function toggleHoverEffect() {
+//     clock.classList.toggle('hover-effect');
+// }
+
+// // Apply the hover effect every second
+// setInterval(toggleHoverEffect, 1000);
+// console.log ()
+
+
